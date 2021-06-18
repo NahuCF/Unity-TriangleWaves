fd7f29c8ce8fb0f90226d05b9988e0c8c47019fc
Nahuel Christian Fernandez <nahuefer105@outlook.com>
2021-06-17 22:49:15 -0300

    You can move in a snappy way

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D rd;
+    public float speed = 10.0f;
+
+    private Vector2 moveVelocity;
+
+
     void Start()
     {
+        rd = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        moveVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed;
+
         var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void FixedUpdate()
+    {
+        rd.MovePosition(rd.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
